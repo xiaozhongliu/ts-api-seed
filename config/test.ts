1b0777e0410c0f16c6ee0a5ed9bf29ea695b3948
xiaozhongliu <xiaozhong.liu@outlook.com>
2018-08-10 13:02:17 +0800

    update debugging config

--- config/test.ts
+++ config/test.ts
@@ -8,12 +8,12 @@ export default {
         PORT: 6379,
     },
 
-    MONGO: 'mongodb://test:test@127.0.0.1:27017/test',
+    MONGO: 'mongodb://127.0.0.1:27017/test',
 
     POSTGRES: {
-        HOST: '127.0.0.1',
-        BASE: 'test',
-        USER: 'test',
-        PASS: 'test',
+        HOST: '111.231.108.221',
+        BASE: 'usersys',
+        USER: 'usersys',
+        PASS: '123qweASD!1',
     },
 }
