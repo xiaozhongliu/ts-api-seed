12557a7af56f3c8ed57c723ed4347dd11cf21abd
xiaozhongliu <xiaozhong.liu@outlook.com>
2018-09-24 09:34:16 +0800

    refactor mailer util

--- util/mailer.ts
+++ util/mailer.ts
@@ -24,28 +24,15 @@ export default {
     alarm(appName: string, partialBody: string) {
         if (!MAILER_ON) return
 
-        const entireBody = `<span style="font-family: 'Microsoft YaHei'"><b>错误详情:</b> ${partialBody}</span>`
+        const body = `<span style="font-family: 'Microsoft YaHei'"><b>Error Detail:</b> ${partialBody}</span>`
         const mailOptions = {
             from: `"${MAILER.NICK_NAME}" <${MAILER.USER}>`,
             to: MAILER.RECEIVERS.join(','),
-            subject: `[${getEnv()}]应用[${appName}]出错`,
-            html: entireBody,
+            subject: `[${process.env.NODE_ENV}] [${appName}] Service Error`,
+            html: body,
         }
         transporter.sendMail(mailOptions, (err, info) => {
             err && console.log('MAIL ALARM FAIL:   ', err)
         })
     },
-}
-
-function getEnv() {
-    const { NODE_ENV } = process.env
-    if (!NODE_ENV) return '开发环境'
-    return envMap[NODE_ENV]
-}
-
-const envMap: Indexed = {
-    dev: '开发环境',
-    int: '测试环境',
-    qa: 'QA环境',
-    prod: '生产环境',
 }
